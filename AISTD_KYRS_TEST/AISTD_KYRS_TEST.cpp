53aa41a4b095aca9a3d25273ef96818905ef4d45
VlasovEgor <73856832+VlasovEgor@users.noreply.github.com>
2020-12-27 16:50:39 +0300

    Add files via upload

---
+++ AISTD_KYRS_TEST/AISTD_KYRS_TEST.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../AISTD_KYRS1.3/AISTD_KYRS1.3.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace AISTDKYRSTEST
+{
+	TEST_CLASS(AISTDKYRSTEST)
+	{
+	public:
+	};
+	TEST_METHOD(test_1)
+	{
+		Queue* elements;
+		cout << "(1+4)*3";
+		double res = calculation(elements);
+		Assert::AreEqual(res,15);
+	}
+	TEST_METHOD(test_2)
+	{
+		Queue* elements;
+		cout << "(2+7)/(1+2)";
+		double res = calculation(elements);
+		Assert::AreEqual(res, 3);
+	}
+	TEST_METHOD(test_3)
+	{
+		Queue* elements;
+		cout << "25+65";
+		double rest = calculation(elements);
+		Assert::AreEqual(res, 90);
+	}
+	TEST_METHOD(test_4)
+	{
+		Queue* elements;
+		cout << "2^5";
+		double res = calculation(elements);
+		Assert::AreEqual(res, 32);
+	}
+}
